cbfe1fececf94d72843bf3960e036e0ea28c0b71
Rsiesener <r.siesener91@gmail.com>
2021-10-11 23:04:02 -0500

    Tic Tac Toe game creation.
    Built HTML. Created api.js, ui.js, events.js, and app.js.
    Added to and finished app.js. Added to and did not finish events.js.

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 // user require with a reference to bundle the file and use it in this file
 // var example = require('./example');
 
-import 'bootstrap'
-
 // allows usage of new JS features
 require('babel-polyfill')
 
